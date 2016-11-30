0cb17c532e2a0aefcfa878020d322d168fa33f3a
miles <yanlang13@gmail.com>
2016-12-01 00:01:56 +0800

    commit all

--- src/app/bar-graph.directive.ts
+++ src/app/bar-graph.directive.ts
@@ -6,19 +6,37 @@ import * as d3 from 'd3';
 export class BarGraph {
     graphWidth: number = 500;
     graphHeight: number = 400;
-    divs: any;
+    graph: any;
     data: Array<number> = [10, 20, 30, 40, 60];
     constructor(
         el: ElementRef, renderer: Renderer) {
         // renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'yellow');
 
         //graph is a selection type
-        var graph = d3.select(el.nativeElement);
-        //append svg是為了透過attr改變view(CSS可連動),if style則無法透過css覆寫
-        graph.append('svg')
-            .attr('class', 'chart')
-            .attr('width', this.graphWidth + 'px')
-            .attr('height', this.graphHeight + 'px').selectAll('div')
-            .data(this.data).enter().append('div');
+        //append svg是為 了透過attr改變view(CSS可連動),if style則無法透過css覆寫
+        var graph = d3.select(el.nativeElement).append('svg');
+        var xScale = d3.scaleLinear()
+            .domain([0, d3.max(this.data)])
+            .range([0, this.graphWidth]);
+
+        graph.selectAll('rect').attr('class', 'rect')
+            .data(this.data)
+            .enter().append('rect')
+            .attr('x', 0)
+            .attr('y', 100)
+            .attr('width', 100)
+            .attr('height', 100)
+            .attr('fill', 'grey');
+        // graph.append('svg')
+        //     .attr('class', 'chart')
+        //     .attr('width', this.graphWidth + 'px')
+        //     .attr('height', this.graphHeight + 'px').selectAll('div')
+        //     .data(this.data).enter().append('div')
+        //     .attr('width', 10 + 'px');
+        graph.append('svg').append('circle')
+            .attr('cx', 5)
+            .attr('cy', 5)
+            .attr('r', 5);
     }
+
 }
