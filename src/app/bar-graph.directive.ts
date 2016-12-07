2e598611ce1ddd0da2f1bdfc12bb558e02f182ab
miles <yanlang13@gmail.com>
2016-12-07 20:37:46 +0800

    commit all

--- src/app/bar-graph.directive.ts
+++ src/app/bar-graph.directive.ts
@@ -1,51 +1,63 @@
 import { Directive, ElementRef, Input, Renderer, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 
+let frame;
+let canvas;
+let width = 500;
+let height = 300;
+let margin = { top: 20, right: 20, bottom: 20, left: 20 };
+let xScale;
+let yScale;
+let xAxis;
+let yAxis;
 
 @Directive({ selector: 'vertical-bar-graph' })
 export class BarGraph implements OnInit {
-    private frame;
-    private canvas;
-    private width = 400;
-    private height = 200;
-    private margin = { top: 20, right: 20, bottom: 40, left: 40 };
 
-    private xScale;
-    private yScale;
-    private xAxis;
-    private yAxis;
-    private dataPath: string = 'app/data/circle.json';
+    private dataPath: string = 'app/data/bar.json';
 
     constructor(private el: ElementRef, private renderer: Renderer) {
         //append svg是為 了透過attr改變view(CSS可連動),if style則無法透過css覆寫
-        this.frame = d3.select(el.nativeElement).append('svg')
-            //frame留白
-            .attr('width', this.width + this.margin.left + this.margin.right)
-            .attr('height', this.height + this.margin.top + this.margin.bottom);
-        this.canvas = this.frame.append('g')
-            .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');;
-        // .attr('width', this.width - this.margin.right - this.margin.left)
-        // .attr('height', this.height - this.margin.top - this.margin.bottom);
+        //frame留白
+        frame = d3.select(el.nativeElement).append('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom);
+        canvas = frame.append('g')
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');;
     }
 
     ngOnInit(): void {
         this.setup();
-        this.buildGraph();
-        // this.drawCircles(this.canvas);
+        this.buildContent();
+        this.drawCircles();
     }
 
     setup(): void {
-        this.xScale = d3.scaleOrdinal().range([0, this.width]);
-        this.yScale = d3.scaleLinear().range([this.height, 0]);
+        xScale = d3.scaleBand().range([0, width]);
+        yScale = d3.scaleLinear().range([0, height]);
     }
 
-    buildGraph(): void {
-
-    }
-
-    drawCircles(svg): void {
+    buildContent(): void {
         d3.json(this.dataPath, function (data) {
-            svg.selectAll('circle').data(data).enter().append('circle')
+            console.log(JSON.stringify(data));
+            // console.log(d3.max(data, (d) => d['value']));
+            xScale.domain(data.map((d) => d['name']));
+            yScale.domain([d3.max(data, (d) => d['value']), 0]);
+
+            canvas.selectAll('rect').data(data).enter().append('rect')
+                .attr('x', (d) => xScale(d['name']))
+                .attr('y', (d) => yScale(d['value']))
+                .attr('width', xScale.bandwidth())
+                .attr('height', (d) => height - yScale(d['value']))
+                // .attr("height", (d) => yScale(d['value']))
+                .attr('fill', 'grey');
+
+        });
+    }
+
+    drawCircles(): void {
+        d3.json('app/data/circle.json', function (data) {
+            canvas.selectAll('circle').data(data).enter().append('circle')
                 .attr('cx', (d) => d['x_axis'])
                 .attr('cy', (d) => d['y_axis'])
                 .attr('r', (d) => d['radius']);
