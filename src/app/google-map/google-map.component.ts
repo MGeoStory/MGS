f19609c118e0ec7dfad01972d279eb30ff0b6d75
miles <yanlang13@gmail.com>
2016-12-08 16:40:04 +0800

    commit all

--- src/app/google-map/google-map.component.ts
+++ src/app/google-map/google-map.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+// import { GoogleMapsLoader } from 'google-maps';
+import * as d3 from 'd3';
 
+// import { GoogleMapAPI } from 'googlemaps';
+// import * as aa from 'googlemaps';
 @Component({
     selector: 'app-google-map',
     templateUrl: 'app-google-map.component.html',
     styleUrls: ['app-google-map.component.css']
 })
-export class GoogleMapComponent {
+export class GoogleMapComponent implements OnInit, OnChanges {
     title: string = 'google map';
     lat: number = 23.5;
     lng: number = 121.5;
+
+    ngOnInit(): void {
+        var map = new google.maps.Map(document.getElementById('map'),
+            {
+                zoom: 7,
+                center: { lat: 41.85, lng: -87.65 }
+
+            });
+    }
+
+    ngOnChanges(): void {
+
+    }
+
 }
