8819dd2d667efeaf8f037696b5d14fcfe291de95
miles <yanlang13@gmail.com>
2017-01-04 21:02:01 +0800

    clean all

--- src/app/leaflet-map/app-leaflet-map.component.ts
+++ src/app/leaflet-map/app-leaflet-map.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
+
+let map;
+let mapboxOSM: string;
+let mapboxId: string;
+let mapboxToken: string;
 @Component({
     selector: 'app-leaflet-map',
     templateUrl: 'app-leaflet-map.component.html',
     styleUrls: ['app-leaflet-map.component.css']
-}) export class LeafletMapComponent {
+}) export class LeafletMapComponent implements OnInit {
     title: string = 'Leaflet Map';
-    //Map container not found.
-    //This problem occurs when L.map('map') is called before the DOM has finished loading.
-    // map = L.map('leafMap').setView([51, 51], 13);
+    map = L.map('lmap').setView([51, 0], 13);
+
+    ngOnInit() {
+    }
 } 
