ea90339f949baf61375baf9c64155fded71ca6fc
miles <yanlang13@gmail.com>
2017-01-04 21:41:49 +0800

    commit all

--- src/app/leaflet-map/app-leaflet-map.component.ts
+++ src/app/leaflet-map/app-leaflet-map.component.ts
@@ -2,11 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import * as d3 from 'd3';
 
-
 let map;
-let mapboxOSM: string;
-let mapboxId: string;
-let mapboxToken: string;
+
 @Component({
     selector: 'app-leaflet-map',
     templateUrl: 'app-leaflet-map.component.html',
@@ -15,9 +12,17 @@ let mapboxToken: string;
     title: string = 'Leaflet Map';
 
     ngOnInit() {
-        console.log(document.getElementById('lmap'));
-
-        map = L.map(document.getElementById('lmap')).setView([51, 0], 13);
-        // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}').addTo(map);
+        d3.select('div')
+            .attr('id', 'lmap')
+            .style('width', 600 + 'px')
+            .style('height', 400 + 'px');
+        map = L.map('lmap').setView([51, 0], 13);
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery c <a href="http://mapbox.com">Mapbox</a>',
+            id: 'mapbox.streets'
+        }).addTo(map);
     }
 } 
