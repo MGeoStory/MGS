6bfbbb9a60a4f47b7a98d5406f8eebfb265bc8a5
miles <yanlang13@gmail.com>
2017-01-04 19:05:15 +0800

    commit all

--- src/app/leaflet-map/app-leaflet-map.component.ts
+++ src/app/leaflet-map/app-leaflet-map.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as L from 'leaflet';
 
 @Component({
     selector: 'app-leaflet-map',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
     styleUrls: ['app-leaflet-map.component.css']
 }) export class LeafletMapComponent {
     title: string = 'Leaflet Map';
-}
+    //Map container not found.
+    //This problem occurs when L.map('map') is called before the DOM has finished loading.
+    // map = L.map('leafMap').setView([51, 51], 13);
+} 
