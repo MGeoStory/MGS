285a06b729ffd62a2d483c9b17b8a7c13ada0733
miles <yanlang13@gmail.com>
2017-01-06 00:52:04 +0800

    commit all

--- src/app/leaflet-map/app-leaflet-map.component.ts
+++ src/app/leaflet-map/app-leaflet-map.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import * as d3 from 'd3';
 
-let map;
+let map: L.Map;
+let nullBound: L.LatLngBounds;
+
 
 @Component({
     selector: 'app-leaflet-map',
@@ -14,8 +16,37 @@ let map;
 
     ngOnInit() {
         d3.select('div').attr('id', 'lmap');
-        map = L.map('lmap').setView([51, 0], 13);
+        map = L.map('lmap').setView([0, 0], 5);
+        L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlsZXN3YW5nIiwiYSI6ImNpeGl2NDF1ejAwMTAycWw4cDhoanViaGMifQ.nwPu50GsqxfjSc1t7EsVZA', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        }).addTo(map);
 
 
-    }
+        d3.json("app/data/stations.json", function (error, data) {
+            if (error) throw error;
+            nullBound = L.latLngBounds(null, null);
+            d3.entries(data).forEach(function (d) {
+                d.value.lat_lng = L.latLng([d.value[1], d.value[0]]);
+                var circle = L.circle(d.value.lat_lng, {
+                    radius: 200,
+                }).addTo(map);
+                // map.addLayer(circle); //也可
+
+                // fit the circles
+                var bounds = nullBound.extend(d.value.lat_lng);
+                map.fitBounds(bounds);
+                circle.on('mouseover', function (d) {
+                    var layer = d.target;
+                    console.log(d.target);
+                    layer.setStyle({ color: 'red' });
+                    // console.log(layer.feature);
+                    //geojson 專用
+                    // console.log(layer.feature.properties);
+                })
+            });
+
+
+        });//END OF d3.json
+
+    }//END OF ngOnInit
 } 
