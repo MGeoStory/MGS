47088b9d6688c2af2a37f184f2bf1d9629e9b242
miles <yanlang13@gmail.com>
2017-01-05 21:02:01 +0800

    commit all

--- src/app/leaflet-map/app-leaflet-map.component.ts
+++ src/app/leaflet-map/app-leaflet-map.component.ts
@@ -8,21 +8,14 @@ let map;
     selector: 'app-leaflet-map',
     templateUrl: 'app-leaflet-map.component.html',
     styleUrls: ['app-leaflet-map.component.css']
+
 }) export class LeafletMapComponent implements OnInit {
     title: string = 'Leaflet Map';
 
     ngOnInit() {
-        d3.select('div')
-            .attr('id', 'lmap')
-            .style('width', 600 + 'px')
-            .style('height', 400 + 'px');
+        d3.select('div').attr('id', 'lmap');
         map = L.map('lmap').setView([51, 0], 13);
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery c <a href="http://mapbox.com">Mapbox</a>',
-            id: 'mapbox.streets'
-        }).addTo(map);
+
+
     }
 } 
