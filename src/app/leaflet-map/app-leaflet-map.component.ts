0b6586dc5c35c77e75e30ec41691fa55fde4d968
miles wang <yanlang13@gmail.com>
2017-01-12 00:56:50 +0800

    commit all

--- src/app/leaflet-map/app-leaflet-map.component.ts
+++ src/app/leaflet-map/app-leaflet-map.component.ts
@@ -4,9 +4,11 @@ import * as d3 from 'd3';
 
 let map: L.Map;
 let nullBound: L.LatLngBounds;
-let mapboxUrl:string = 'https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlsZXN3YW5nIiwiYSI6ImNpeGl2NDF1ejAwMTAycWw4cDhoanViaGMifQ.nwPu50GsqxfjSc1t7EsVZA';
-let mapboxAttribution :string ='Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';
-let mapbox:L.TileLayer;
+let mapboxUrl: string = 'https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlsZXN3YW5nIiwiYSI6ImNpeGl2NDF1ejAwMTAycWw4cDhoanViaGMifQ.nwPu50GsqxfjSc1t7EsVZA';
+let mapboxAttribution: string = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';
+let mapbox: L.TileLayer;
+let overlaySVG = d3.select('null');
+let g = d3.select('null');
 
 @Component({
     selector: 'app-leaflet-map',
@@ -15,19 +17,67 @@ let mapbox:L.TileLayer;
 
 }) export class LeafletMapComponent implements OnInit {
     title: string = 'Leaflet Map';
-    
+
     ngOnInit() {
+        //create mapbox and tileLayer
         d3.select('div').attr('id', 'lmap');
         map = L.map('lmap').setView([0, 0], 5);
 
-        mapbox= L.tileLayer(mapboxUrl, {
+        mapbox = L.tileLayer(mapboxUrl, {
             attribution: mapboxAttribution,
         });
-
         map.addLayer(mapbox);
 
+        //create country of TW
+        overlaySVG = d3.select(map.getPanes().overlayPane).append('svg');
+        g = overlaySVG.append('g').attr('class', 'leaflet-zoom-hide');
 
 
+        var geojsonFeature = {
+            "type": "Feature",
+            "properties": {
+                "name": "Coors Field",
+                "amenity": "Baseball Stadium",
+                "popupContent": "This is where the Rockies play!"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-104.99404, 39.75621]
+            }
+        };
+        L.geoJSON(geojsonFeature).addTo(map);
+        // L.geoJSON().addTo(map).addData(geojsonFeature);
+        // var test = require('app/data/OUNTY_stoneman-ms.json');
+        // console.log(test);
+        var myLines;
+
+        myLines = [{
+            "type": "LineString",
+            "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+        }, {
+            "type": "LineString",
+            "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+        }];
+
+        var polygon;
+
+        polygon = [{
+            "type": "Polygon",
+            "coordinates": [
+                [[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]]
+            ]
+        }];
+
+
+        //useing d3.json to read file and addTo leaflet map
+        d3.json('app/data/COUNTY_stoneman-ms.json', function (data) {
+            console.log(JSON.stringify(data));
+            
+            L.geoJSON().addTo(map).addData(data);
+            console.log('added');
+        });
+
+        //add circle
         d3.json("app/data/stations.json", function (error, data) {
             if (error) throw error;
             nullBound = L.latLngBounds(null, null);
@@ -50,9 +100,11 @@ let mapbox:L.TileLayer;
                     // console.log(layer.feature.properties);
                 })
             });
-
-
         });//END OF d3.json
-
     }//END OF ngOnInit
-} 
+
+
+
+
+
+} //END OF export
