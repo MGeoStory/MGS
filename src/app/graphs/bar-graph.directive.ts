a91802d9fbf9aa6b541d6fa8bd99bbf3e40bbd00
m-geo-story <m-geo-story@gmail.com>
2017-02-11 19:04:46 +0800

    copy app-leaflet-map to bar-graph 1.parse data smartly (no $A:object{key,value}) 2. bug in countryID X

--- src/app/graphs/bar-graph.directive.ts
+++ src/app/graphs/bar-graph.directive.ts
@@ -28,12 +28,13 @@ export class BarGraph implements OnInit {
                 userClickedInfo = info;
             });
         gc.createCanvas(el.nativeElement);
-        
-}//END OF constructor
+
+    }//END OF constructor
 
     ngOnInit(): void {
         this.setup();
-        this.drawContent();
+        // this.drawContent();
+        this.drawGraph();
         // this.drawXAxis();
         // this.drawCircles();
     }//END OF ngOnInit
@@ -47,13 +48,96 @@ export class BarGraph implements OnInit {
         xAxis = d3.axisBottom(gc.xScaleBand);
     }//END OF setup
 
+    drawGraph(): void {
+        console.log('drawGraph');
+        var parseTime = d3.timeParse("%Y/%m/%d");
+        d3.csv('app/data/rawdata/simpleTest.csv', (data) => {
+            //1. filter data
+            var dataFiltered = data
+                .filter(column => {
+                    if (column['發票年月'] == '2013/01/01' || column['行業名稱'] == '便利商店') {
+                        return column;
+                    }
+                });
+            //2. adjust format
+            dataFiltered.forEach(d => {
+                //deal time and numbers format
+                d['發票年月'] = parseTime(d['發票年月']);
+                d['平均客單價'] = +d['平均客單價'];
+                d['平均開立張數'] = +d['平均開立張數'];
+                d['平均開立金額'] = +d['平均開立金額'];
+            });
+
+            //the paras in d3.extent() is array[], so build up a simple array
+            var valuesOfData;
+
+            valuesOfData = dataFiltered.map((d) => {
+                return {
+                    countryID: d['縣市代碼'],
+                    value: d['平均客單價']
+                }
+            });
+            var extentOfData = d3.extent(valuesOfData, function (d) {
+                return d['value'];
+            });
+            console.log(extentOfData);
+
+            // console.log(dataFiltered);
+            //3. nest data by縣市代碼
+            var dataNested = d3.nest()
+                .key(d => { return d['縣市代碼'] })
+                .entries(dataFiltered);
+            // console.log(dataNested);
+
+            //4. map data(make data simplify) by what the map need
+            var dataMapped = dataNested.map((d) => {
+                // console.log(d.key);
+                // console.log(d.values[0]['平均客單價']);
+                return {
+                    key: d.key,
+                    value: d.values[0]['平均客單價']
+                }
+            });
+
+            //5.using key to select value of data
+            var dataDealed = d3.map(dataMapped, (d) => {
+                return d.key;
+            });
+            // console.log(dataDealed.keys());
+            var extentOfData = d3.extent(valuesOfData, function (d) {
+                return d['value'];
+            });
+            gc.xScaleBand.domain(dataDealed.keys());
+            gc.yScaleLinear.domain(extentOfData);
+            gc.canvas.selectAll('rect').data(dataMapped).enter().append('rect')
+                .attr('x', (d) => gc.xScaleBand(d['key']))
+                .attr('y', (d) => gc.yScaleLinear(d['value']))
+                .attr('width', gc.xScaleBand.bandwidth())
+                .attr('height', (d) => gc.getFrameHeight() - gc.yScaleLinear(d['value']))
+                // .attr("height", (d) => yScale(d['value']))
+                .attr('fill', 'grey');
+
+            gc.canvas.selectAll('text').data(dataMapped).enter().append('text')
+                .attr('class', 'bar-value')
+                .attr('x', (d) => gc.xScaleBand(d['key']) + gc.xScaleBand.bandwidth() / 2)
+                .attr('y', (d) => gc.yScaleLinear(d['value']) - 5)
+                .attr('text-anchor', 'middle')
+                .text((d) => d['value']);
+                
+            gc.canvas.append('g')
+                .attr('class', 'xAxis')
+                .attr('transform', `translate(0,${gc.getFrameHeight()})`)
+                .call(xAxis);
+        });
+    }
+
     drawContent(): void {
         d3.json(this.dataPath, function (data) {
             // console.log(JSON.stringify(data));
             // console.log(d3.max(data, (d) => d['value']));
             gc.xScaleBand.domain(data.map((d) => d['name']));
             gc.yScaleLinear.domain([d3.max(data, (d) => d['value']), 0]);
-            
+
             //bar-rect
             gc.canvas.selectAll('rect').data(data).enter().append('rect')
                 .attr('x', (d) => gc.xScaleBand(d['name']))
