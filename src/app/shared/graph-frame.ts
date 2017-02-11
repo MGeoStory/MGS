3531845be313266cfda4a2bb9a63dd82bd59e2bf
m-geo-story <m-geo-story@gmail.com>
2017-02-11 09:33:43 +0800

    Responsive SVG

--- src/app/shared/graph-frame.ts
+++ src/app/shared/graph-frame.ts
@@ -8,7 +8,8 @@ export class GraphFrame {
     margin: { top: number, right: number, bottom: number, left: number };
 
     constructor() {
-        this.width = 300;
+        //phone6s size: 414x736
+        this.width = 414;
         this.height = 300;
 
         //nargin.top and margin.left are used in Frame and Canvas.
@@ -23,8 +24,17 @@ export class GraphFrame {
     setFrame(htmlElement: any): d3.Selection<any, any, any, any> {
         //append svg是為 了透過attr改變view(CSS可連動),if style則無法透過css覆寫
         //frame留白
+        //http://www.oxxostudio.tw/articles/201409/svg-23-viewpoint-viewBox.html
+        let width = this.width + this.margin.left + this.margin.right;
+        let height = this.height + this.margin.top + this.margin.bottom;
         return this.frame = d3.select(htmlElement).append('svg')
-            .attr('width', this.width + this.margin.left + this.margin.right)
-            .attr('height', this.height + this.margin.top + this.margin.bottom);
+            .attr('width', '100%')
+            .attr('height', height)
+            .attr("preserveAspectRatio", "xMidYMid meet")
+            .attr("viewBox", "0 0 " + width + " " + height);
+
+        // return this.frame = d3.select(htmlElement).append('svg')
+        //     .attr('width', this.width + this.margin.left + this.margin.right)
+        //     .attr('height', this.height + this.margin.top + this.margin.bottom);
     };
 }
