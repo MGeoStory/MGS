7e157e7787682dce3d58d1089f8d5e8ad928fbef
m-geo-story <m-geo-story@gmail.com>
2017-02-16 23:08:49 +0800

    Promise 1.resolve(put the result) 2.promise.then(res()=>   // res = result putted  );

--- src/app/dropdown-list/dropdown-list.component.ts
+++ src/app/dropdown-list/dropdown-list.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 
+var result: Array<Object>;
 
 @Component({
     selector: 'app-dropdown-list',
     templateUrl: 'dropdown-list.component.html',
     styleUrls: ['dropdown-list.component.css']
-}) export class DropdownList {
-    title = 'data sharing';   
+}) export class DropdownList implements OnInit {
+    title = 'data sharing';
     brief = 'infomation about Taiwan.';
+
+    ngOnInit() {
+
+        
+        var t = this.readFile().then(function (res) {
+            console.log(res);
+        })
+        console.log(t);
+
+
+    }// END OF ngOnInit
+
+    /**
+     * 
+     */
+
+    readFile() {
+        return new Promise(function (resolve, reject) {
+            d3.csv('app/data/rawdata/simpleTest.csv', (data: Array<Object>) => {
+                result = data.filter(column => {
+                    if (column['發票年月'] == '2013/01/01' || column['行業名稱'] == '便利商店') {
+                        return column;
+                    }
+                });
+                resolve(result);
+            });
+        });
+    }
+
+    filterData(): void {
+        d3.csv('app/data/rawdata/simpleTest.csv', (data: Array<Object>) => {
+            result = data.filter(column => {
+                if (column['發票年月'] == '2013/01/01' || column['行業名稱'] == '便利商店') {
+                    return column;
+                }
+            });
+        });
+    }
 };
