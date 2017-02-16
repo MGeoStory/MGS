f0ab00e007ac4ec427e4109bebe02208352e8e67
m-geo-story <m-geo-story@gmail.com>
2017-02-17 00:53:11 +0800

    dropdwon list done (incorrect list)

--- src/app/dropdown-list/dropdown-list.component.ts
+++ src/app/dropdown-list/dropdown-list.component.ts
@@ -13,39 +13,61 @@ var result: Array<Object>;
 
     ngOnInit() {
 
-        
-        var t = this.readFile().then(function (res) {
-            console.log(res);
-        })
-        console.log(t);
+        this.dealedData().then(function (res) {
+            var listOfTime;
+            listOfTime = res.map(function (data) {
+                // console.log(data);
+                return {
+                    time: data['發票年月'],
+                }
+            });
+            console.log(listOfTime);
 
+            var data1 = [ {'value': 10}, {'value': 11}, {'value': 12} ];
+            console.log(data1);
+            var dropDown = d3.select('#select_table').append('select')
+                .attr('name','listOfTime');
+            var options = dropDown.selectAll("option")
+                .data(listOfTime) 
+                .enter()
+                .append("option");
+            options.text(function(d){
+                console.log(d);
+                return 11;
+            }).attr('value',11);
+            
+        });
 
+        var t = this.dealedData().then(function (res) {
+        });
     }// END OF ngOnInit
 
-    /**
-     * 
-     */
 
-    readFile() {
+    /**
+     * filter and parse data values
+     */
+    dealedData(): Promise<Array<Object>> {
         return new Promise(function (resolve, reject) {
+            var parseTime = d3.timeParse("%Y/%m/%d");
             d3.csv('app/data/rawdata/simpleTest.csv', (data: Array<Object>) => {
-                result = data.filter(column => {
+                data.filter(column => {
                     if (column['發票年月'] == '2013/01/01' || column['行業名稱'] == '便利商店') {
                         return column;
                     }
                 });
-                resolve(result);
-            });
-        });
-    }
 
-    filterData(): void {
-        d3.csv('app/data/rawdata/simpleTest.csv', (data: Array<Object>) => {
-            result = data.filter(column => {
-                if (column['發票年月'] == '2013/01/01' || column['行業名稱'] == '便利商店') {
-                    return column;
-                }
-            });
-        });
-    }
+                data.forEach(d => {
+                    //deal time and numbers format
+                    d['發票年月'] = parseTime(d['發票年月']);
+                    d['平均客單價'] = +d['平均客單價'];
+                    d['平均開立張數'] = +d['平均開立張數'];
+                    d['平均開立金額'] = +d['平均開立金額'];
+                });
+
+                result = data;
+
+                resolve(result);
+            });// END OF d3.csv
+        });// END OF return
+    }// END OF dealedData
 };
