0f2811735f8f77cdc698feb0728191bfd61c8658
m-geo-story <m-geo-story@gmail.com>
2017-03-05 00:17:50 +0800

    listen all select condition from user

--- src/app/dropdown-list/dropdown-list.component.ts
+++ src/app/dropdown-list/dropdown-list.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 import { MapGraphService } from 'app/shared/map-graph.service';
 
-let dropDataOfTime : Array<string> = [];
+let dropDataOfTime: Array<string> = [];
+let dropDataOfType: Array<string> = [];
 let thisComponent: DropdownList;
 let dataFormatted: Array<Object>;
 let items: Array<String>;
@@ -13,10 +14,13 @@ let items: Array<String>;
     styleUrls: ['dropdown-list.component.css']
 }) export class DropdownList implements OnInit {
     //ng variables
-    public itemsOfTime:Array<string> = [];
-    public activeOfTime:Array<string> =[];
-    public itemsOfType:Array<string> = [];
-    public activeOfype:Array<string> =[];
+    public itemsOfTime: Array<string> = [];
+    public activeOfTime: Array<string> = [];
+    public itemsOfType: Array<string> = [];
+    public activeOfType: Array<string> = [];
+
+    public selecedOfTime: string;
+    public selectedOfType: string;
 
     title = 'data sharing';
     brief = 'infomation about Taiwan.';
@@ -30,19 +34,39 @@ let items: Array<String>;
     }// END OF ngOnInit
 
     /**
-     * ng-select
+     * get ng-select info
      */
-    timeSelected(value:any):void{
-        console.log(value.text);
-        thisComponent.filteredData(value.text, dataFormatted);
+    timeSelected(value: any): void {
+        this.selecedOfTime = value.text;
+        this.getSelectedCondition();
     }
+
     /**
-     * ng-select
+     * get ng-select info
      */
-    typeSelected(value:any):void{
-        console.log(value.text);
-        thisComponent.filteredData(value.text, dataFormatted);
+    typeSelected(value: any): void {
+        this.selectedOfType = value.text;
+        this.getSelectedCondition();
     }
+
+    getTimeSelected(): string {
+        return this.selecedOfTime;
+    }
+
+    getTypeSelected(): string {
+        return this.selectedOfType;
+    }
+
+    /**
+     * deal the condition about user's select
+     */
+    getSelectedCondition() {
+        let time = this.getTimeSelected();
+        let type = this.getTypeSelected();
+        console.log(time+""+type);
+        thisComponent.filterData(time, type, dataFormatted);
+    }
+
     /**
      * deal data for dropdwon list and Map/Graph
      */
@@ -53,6 +77,7 @@ let items: Array<String>;
             let parseTime = d3.timeParse("%Y/%m/%d");
             // data manipulation: http://learnjsdata.com/group_data.html
             d3.csv('app/data/rawdata/simpleTest.csv', (data: Array<Object>) => {
+
                 let listOfTime = d3.nest()
                     .key(d => { return d['發票年月'] })
                     .entries(data);
@@ -60,6 +85,15 @@ let items: Array<String>;
                     dropDataOfTime.push(d.key);
                 })
                 dropDataOfTime.sort();
+
+                let listOfType = d3.nest()
+                    .key(d => { return d['行業名稱'] })
+                    .entries(data);
+                listOfType.forEach(d => {
+                    dropDataOfType.push(d.key);
+                })
+                dropDataOfType.sort();
+
                 data.forEach(d => {
                     //deal time and numbers format
                     // d['發票年月'] = parseTime(d['發票年月']);
@@ -70,53 +104,35 @@ let items: Array<String>;
 
                 dataFormatted = data;
                 // console.log(dropDataOfTime[0]);
-                let defaultSelected = dropDataOfTime[0];
+                let defaultSelectedOfTime = dropDataOfTime[0];
+                let defaultSelectedOfType = dropDataOfType[0];
                 // console.log(data);
-                thisComponent.filteredData(defaultSelected, dataFormatted);
+
+                //set the values of ng-select
                 thisComponent.activeOfTime = [dropDataOfTime[0]];
                 thisComponent.itemsOfTime = dropDataOfTime;
+                thisComponent.selecedOfTime = dropDataOfTime[0];
+                
+                thisComponent.activeOfType = [dropDataOfType[0]];
+                thisComponent.itemsOfType = dropDataOfType;
+                thisComponent.selectedOfType =dropDataOfType[0];
+                
+                thisComponent.filterData(defaultSelectedOfTime, defaultSelectedOfType, dataFormatted);
                 resolve(data);
             });//END of d3.csv
         });//END of return
     }// END OF dropOfData
 
-    
-
     /**
      * filter array values and annnoumceRefData
      */
-    filteredData(selected: string, data: Array<Object>) {
+    filterData(timeSelected: string, typeSelected: string, data: Array<Object>) {
         let dataFiltered = data.filter(column => {
-            if (column['發票年月'] == selected && column['行業名稱'] == '便利商店') {
+            if (column['發票年月'] == timeSelected && column['行業名稱'] == typeSelected) {
                 return column;
             }
         })
-        // console.log(dataFiltered);
+        console.log(dataFiltered);
         thisComponent.mgs.announceRefData(dataFiltered);
     }//END of filteredData
-
-    /**
-     * call after setDropData(), set the droplist value and text, and call filteredData() when menu on change
-     */
-    setDropdownList() {
-        //select id => '#+id'
-        var dropDown = d3.select('#dropdown_year').attr('name', 'listOfTime');
-
-        // html select->options
-        var options = dropDown.selectAll('option')
-            .data(dropDataOfTime)
-            .enter()
-            .append('option');
-        options
-            .text((d, i) => { return dropDataOfTime[i]; })
-            .attr('value', (d, i) => { return dropDataOfTime[i]; });
-
-        dropDown.on('change', function () {
-            console.log(d3.select(this).property('value'));
-            let selected = d3.select(this).property('value');
-            thisComponent.filteredData(selected, dataFormatted);
-            // thisComponent.mgs.announceRefYear(select); 
-
-        });
-    }//END of setDropdownList
 };// END of Class
