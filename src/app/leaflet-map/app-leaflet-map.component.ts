7f8b3d10efc74e9e508cffdff38f2a7fb4ab5b0e
m-geo-story <m-geo-story@gmail.com>
2017-03-05 19:11:33 +0800

    add the info of control of leaflet-map

--- src/app/leaflet-map/app-leaflet-map.component.ts
+++ src/app/leaflet-map/app-leaflet-map.component.ts
@@ -2,16 +2,24 @@ import { Component, OnInit, OnChanges } from '@angular/core';
 import { MapGraphService } from 'app/shared/map-graph.service';
 import { LMapSetting } from 'app/shared/lmap-setting';
 import { Subscription } from 'rxjs/Subscription';
+import { ViewEncapsulation } from '@angular/core';
 import * as L from 'leaflet';
 import * as d3 from 'd3';
 // import * as rgbHex from 'rgb-hex';
 const rgbHex = require('rgb-hex');
 
 let thisComponent: LeafletMapComponent;
-let map: L.Map;
-let geoJSON: L.GeoJSON;
-let valueOfFeatures: d3.Map<{}>;
+
 let title: string;
+let map: L.Map;
+
+//about laafmap
+let layerOfGeoJSON: L.GeoJSON;
+let featuresClicked: L.FeatureGroup;
+let divOfInfoControl: HTMLElement;
+
+//about data
+let valueOfFeatures: d3.Map<{}>;
 
 //function for color feature by values
 let colorFeature: d3.ScaleLinear<any, any>;
@@ -19,7 +27,8 @@ let colorFeature: d3.ScaleLinear<any, any>;
 @Component({
     selector: 'app-leaflet-map',
     styleUrls: ['app-leaflet-map.component.css'],
-    templateUrl: 'app-leaflet-map.component.html'
+    templateUrl: 'app-leaflet-map.component.html',
+    encapsulation: ViewEncapsulation.None //no shoadow DOM.
 
 }) export class LeafletMapComponent implements OnInit {
     constructor(private mgs: MapGraphService, private lms: LMapSetting) {
@@ -30,7 +39,7 @@ let colorFeature: d3.ScaleLinear<any, any>;
         this.initialMap();
         thisComponent.mgs.refData.subscribe(
             data => {
-                // thisComponent.mappingMap();
+                thisComponent.updateInfoControl(null);
                 thisComponent.getFeatureInfo(data);
                 thisComponent.mappingMap();
             }
@@ -45,14 +54,51 @@ let colorFeature: d3.ScaleLinear<any, any>;
         d3.select('#leaf-map').attr('id', 'lmap');
         map = L.map('lmap').setView([0, 0], 5);
         map.addLayer(thisComponent.lms.basedMap());
+
+        map.addControl(this.createInfoControl());
+
     }// END OF initialMap
+
+    /**
+     * crate control(window) on map
+     */
+    createInfoControl(): L.Control {
+
+        //control will upon leaflet map
+        let infoControl = L.control.attribution({ position: 'topright' });
+        //add HTMLElement
+        infoControl.onAdd = () => {
+            divOfInfoControl = L.DomUtil.create('div');
+            divOfInfoControl.className = 'infoControl';
+            thisComponent.updateInfoControl(null);
+            return divOfInfoControl;
+        };
+        return infoControl;
+    }//.createInfoControl
+
+    /**
+     * deal the control of infomation about user click
+     */
+    updateInfoControl(props) {
+        //the props are the feature
+        if (props != null) {
+            let countryName = props['COUNTYNAME'];
+            //get feature value from data that pass by dropdwon list
+            if (valueOfFeatures.get(props['COUNTYID']) != null) {
+                let valueOfCountry = valueOfFeatures.get(props['COUNTYID'])['value'];
+                divOfInfoControl.innerHTML = `<h4>${countryName}</h4><b>平均客單價：${valueOfCountry}</b>`;
+            } else {
+                divOfInfoControl.innerHTML = `<h4>${countryName}</h4><b>平均客單價：無資料</b>`;
+            }
+        } else {
+            divOfInfoControl.innerHTML = '<h4>click cities to get info</h4>';
+        }
+    }//.updateInfoCOntrol
 
     /**
      * get values from data, pass data to leafMap and draw layers 
      */
     getFeatureInfo(data: Array<Object>): void {
-        // console.log(data);
-        // console.log(data[0]['平均客單價']);
         //extent would read all data[set] and return values
         let extentOfData = d3.extent(data, d => {
             return d['平均客單價'];
@@ -60,7 +106,7 @@ let colorFeature: d3.ScaleLinear<any, any>;
         //why <string>? https://github.com/DefinitelyTyped/DefinitelyTyped/issues/8941
         colorFeature = d3.scaleLinear<string>()
             .domain(extentOfData)
-            .range(["white", "OrangeRed"]);
+            .range(["#FFEDA0", "#800026"]);
 
         //d3.map would create a map<key:object>; the key and object are from data.map
         valueOfFeatures = d3.map(
@@ -74,7 +120,6 @@ let colorFeature: d3.ScaleLinear<any, any>;
                 return d['id']
             }
         );//end of valueOfFeatures
-
     }// end of getFeatureInfo
 
     /**
@@ -83,9 +128,14 @@ let colorFeature: d3.ScaleLinear<any, any>;
     mappingMap(): void {
         //using d3.json to read file and addTo leaflet map
         d3.json('app/data/geojson/tw_country_ms.json', function (data) {
+            //remove the existed layer.
+            if (map.hasLayer(layerOfGeoJSON)) {
+                console.log('111');
+                map.removeLayer(layerOfGeoJSON);
+            }
             // console.log(JSON.stringify(data));
             // let countryID = data['features'][0]['properties']['COUNTYID'];
-            geoJSON = L.geoJSON(data, {
+            layerOfGeoJSON = L.geoJSON(data, {
                 style: thisComponent.styleFeature,
                 //listener event
                 onEachFeature: function (feature, layer) {
@@ -93,15 +143,45 @@ let colorFeature: d3.ScaleLinear<any, any>;
                         click: function (e) {
                             var countyName = feature.properties['COUNTYNAME'];
                             thisComponent.mgs.announceRefId(countyName);
+                            thisComponent.resetHighlightedFeature();
+                            thisComponent.highlightFeature(e);
+                            thisComponent.updateInfoControl(feature.properties);
                         }
-                    });
-                }
+                    });//.layer.on
+                }//.onEachFeature
             });
             //add geoJson and zoom to geoJSON
-            geoJSON.addTo(map);
-            map.fitBounds(geoJSON.getBounds());
+            layerOfGeoJSON.addTo(map);
+            map.fitBounds(layerOfGeoJSON.getBounds());
         });
     }// END OF mappingMap
+
+    /**
+     * rest the highlight feature
+     */
+    resetHighlightedFeature() {
+        if (layerOfGeoJSON.hasLayer(featuresClicked)) {
+            layerOfGeoJSON.resetStyle(featuresClicked);
+        }
+    }
+
+    /**
+     * highlight the feature that user clicked
+     */
+    highlightFeature(e: L.Event) {
+        // console.log(e.target);
+        featuresClicked = e.target;
+        featuresClicked.setStyle({
+            weight: 4,
+            color: '#666',
+            dashArray: '',
+            fillOpacity: 1
+        });
+
+        if (!L.Browser.ie && !L.Browser.edge) {
+            featuresClicked.bringToFront();
+        }
+    }
 
     /**
      * set the style of features
@@ -112,8 +192,9 @@ let colorFeature: d3.ScaleLinear<any, any>;
         return {
             fillColor: thisComponent.getFillColor(countryId),
             fillOpacity: 0.9,
-            color: "gray",
-            weight: 1
+            color: 'gray',
+            dashArray: '3',
+            weight: 1.5
         };
     }//END of styleMap
 
@@ -131,7 +212,7 @@ let colorFeature: d3.ScaleLinear<any, any>;
             return '#' + rgbHex(colorFeature(valueOfCountry));
         } else {
             valueOfCountry = 0;
-            return 'gray';
+            return 'LightGrey';
         }
     }//END OF getFillColor
 } //END OF class
