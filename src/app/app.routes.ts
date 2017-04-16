318880c89757f32d82988c987a54b855bf7eb407
miles <mgeostory@gmail.com>
2017-04-16 10:58:27 +0800

    add # tag in URL

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,4 +16,4 @@ export const routes: Routes = [
     }
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes,{useHash:true});
